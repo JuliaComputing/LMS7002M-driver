f1640cac62f60e4a51cc9ea5757ef7674611ecc1
Tim Besard <tim.besard@gmail.com>
2021-12-16 10:11:45 +0100

    Decode SPI messages.

--- xtrx/litepcie_interface.h
+++ xtrx/litepcie_interface.h
@@ -14,7 +14,13 @@ static inline uint32_t litepcie_interface_transact(void *handle, const uint32_t 
     int *fd = (int *)handle;
 
     //load tx data
-    LMS7_logf(LMS7_TRACE, "SPI request: 0x%08x", data_in);
+    uint16_t addr = (data_in >> 16) & ((1<<15)-1);
+    uint16_t val = data_in & ((1<<16)-1);
+    if (data_in & (1<<31)) {
+        LMS7_logf(LMS7_TRACE, "SPI request: 0x%08x (write 0x%04x at 0x%04x)", data_in, val, addr);
+    } else {
+        LMS7_logf(LMS7_TRACE, "SPI request: 0x%08x (read from 0x%04x)", data_in, addr);
+    }
     litepcie_writel(*fd, CSR_LMS7002M_SPI_MOSI_ADDR, data_in);
 
     //start transaction
@@ -26,7 +32,8 @@ static inline uint32_t litepcie_interface_transact(void *handle, const uint32_t 
     //load rx data
     if (readback) {
         uint32_t data_out = litepcie_readl(*fd, CSR_LMS7002M_SPI_MISO_ADDR) & 0xffff;
-        LMS7_logf(LMS7_TRACE, "SPI reply: 0x%08x", data_out);
+        val = data_out & ((1<<16)-1);
+        LMS7_logf(LMS7_TRACE, "SPI reply: 0x%08x (read 0x%04x)", data_out, val);
         return data_out;
     } else {
         return 0;
