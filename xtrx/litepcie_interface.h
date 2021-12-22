05bf1944612cb67a7e54ad3884e70affed9404e2
Tim Besard <tim.besard@gmail.com>
2021-12-22 08:58:39 +0100

    Don't log SPI messages.

--- xtrx/litepcie_interface.h
+++ xtrx/litepcie_interface.h
@@ -1,5 +1,4 @@
 #include "liblitepcie.h"
-#include <LMS7002M/LMS7002M_logger.h>
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -10,31 +9,12 @@
 #define LITEPCIE_SPI_DONE    (1 << 0)
 #define LITEPCIE_SPI_LENGTH  (1 << 8)
 
-const char *bitstring(int x, int N_bits){
-    static char b[512];
-    char *p = b;
-    b[0] = '\0';
-
-    for(int i=(N_bits-1); i>=0; i--){
-      if (i < N_bits-1 && !((i+1)%8))
-        *p++ = ' ';
-      *p++ = (x & (1<<i)) ? '1' : '0';
-    }
-    return b;
-}
-
 static inline uint32_t litepcie_interface_transact(void *handle, const uint32_t data_in, const bool readback)
 {
     int *fd = (int *)handle;
 
     //load tx data
     uint16_t addr = (data_in >> 16) & ((1<<15)-1);
-    uint16_t val = data_in & ((1<<16)-1);
-    if (data_in & (1<<31)) {
-        LMS7_logf(LMS7_TRACE, "SPI request: 0x%08x (write 0x%04x, or %s, at 0x%04x)", data_in, val, bitstring(val, 16), addr);
-    } else {
-        LMS7_logf(LMS7_TRACE, "SPI request: 0x%08x (read from 0x%04x)", data_in, addr);
-    }
     litepcie_writel(*fd, CSR_LMS7002M_SPI_MOSI_ADDR, data_in);
 
     //start transaction
@@ -45,10 +25,7 @@ static inline uint32_t litepcie_interface_transact(void *handle, const uint32_t 
 
     //load rx data
     if (readback) {
-        uint32_t data_out = litepcie_readl(*fd, CSR_LMS7002M_SPI_MISO_ADDR) & 0xffff;
-        val = data_out & ((1<<16)-1);
-        LMS7_logf(LMS7_TRACE, "SPI reply: 0x%08x (read 0x%04x, or %s)", data_out, val, bitstring(val, 16));
-        return data_out;
+        return litepcie_readl(*fd, CSR_LMS7002M_SPI_MISO_ADDR) & 0xffff;
     } else {
         return 0;
     }
