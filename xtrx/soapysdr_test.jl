517cd00d6ee073afb9001dab4387367989cb0fea
Tim Besard <tim.besard@gmail.com>
2021-12-23 16:05:45 +0100

    Print the final array during testing.

--- xtrx/soapysdr_test.jl
+++ xtrx/soapysdr_test.jl
@@ -13,25 +13,28 @@ SoapySDR.SoapySDRDevice_writeSetting(dev, "TX_PATTERN", "1")
 # open RX streams
 stream = SoapySDR.Stream(ComplexF32, [chan])
 
-function doit(stream)
+function process_buffers(stream)
     SoapySDR.activate!(stream)
 
     try
-        # acquire a single read buffer using the low-level API
+        # acquire buffers using the low-level API
         buffs = Ptr{UInt32}[C_NULL]
-        for i in 1:10
+        bytes = 0
+        for i in 1:100
             bytes, handle, flags, timeNs = SoapySDR.SoapySDRDevice_acquireReadBuffer(dev, stream, buffs)
-            arr = unsafe_wrap(Array, buffs[1], bytes)
             println("Got buffer of size: $(Base.format_bytes(bytes))")
-            # display(arr[1:10])
-            # println("\n ...")
             SoapySDR.SoapySDRDevice_releaseReadBuffer(dev, stream, handle)
         end
+
+        # print last array, for verification
+        arr = unsafe_wrap(Array, buffs[1], bytes)
+        display(arr[1:10])
+        println("\n ...")
     finally
         SoapySDR.deactivate!(stream)
     end
 end
-doit(stream)
+process_buffers(stream)
 
 # close everything
 close(stream)
