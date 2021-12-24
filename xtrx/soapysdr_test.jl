a561dcf80e9e55d92a0a84e7116a525ff0b5977f
Tim Besard <tim.besard@gmail.com>
2021-12-24 09:28:42 +0100

    Show received data rate.

--- xtrx/soapysdr_test.jl
+++ xtrx/soapysdr_test.jl
@@ -7,10 +7,10 @@ dev = open(devs[1])
 # get the RX channel
 chan = dev.rx[1]
 
-# enable loopback
+# enable pattern generator
 SoapySDR.SoapySDRDevice_writeSetting(dev, "TX_PATTERN", "1")
 
-# open RX streams
+# open RX stream
 stream = SoapySDR.Stream(ComplexF32, [chan])
 
 function process_buffers(stream)
@@ -20,11 +20,15 @@ function process_buffers(stream)
         # acquire buffers using the low-level API
         buffs = Ptr{UInt32}[C_NULL]
         bytes = 0
-        for i in 1:100
+        total_bytes = 0
+
+        println("Receiving data")
+        time = @elapsed for i in 1:100
             bytes, handle, flags, timeNs = SoapySDR.SoapySDRDevice_acquireReadBuffer(dev, stream, buffs)
-            println("Got buffer of size: $(Base.format_bytes(bytes))")
             SoapySDR.SoapySDRDevice_releaseReadBuffer(dev, stream, handle)
+            total_bytes += bytes
         end
+        println("Data rate: $(Base.format_bytes(total_bytes / time))/s")
 
         # print last array, for verification
         arr = unsafe_wrap(Array, buffs[1], bytes)
